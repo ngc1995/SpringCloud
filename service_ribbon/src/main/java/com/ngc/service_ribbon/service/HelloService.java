74bcbf966b05e140b37fbf6e48d683c38e0c815b
ngc1995 <ngc123@vip.qq.com>
2018-12-07 14:23:22 +0800

    ribboon hystrix 熔断器

--- service_ribbon/src/main/java/com/ngc/service_ribbon/service/HelloService.java
+++ service_ribbon/src/main/java/com/ngc/service_ribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.ngc.service_ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +10,19 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * @HystrixCommand     该注解对该方法添加了一个熔断器的功能，并指定了fallbackMethod熔断方法为hiError
+     * @param name
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name){
 
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
 
+    public String hiError(String name){
+
+        return "hi,"+name+",error";
+    }
 }
