3790f8d533bd81be93f8eee7d0c2e0e755e832e0
ngc1995 <ngc123@vip.qq.com>
2018-12-07 14:40:12 +0800

    feign hystrix 熔断器

--- service_feign/src/main/java/com/ngc/service_feign/service/SchedualServiceHi.java
+++ service_feign/src/main/java/com/ngc/service_feign/service/SchedualServiceHi.java
@@ -3,7 +3,10 @@ package com.ngc.service_feign.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("service-hi")
+/**
+ * @FeignClient 注解中添加fallback指定熔断后调用的类
+ */
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystrix.class)
 public interface SchedualServiceHi {
 
     @RequestMapping("/hi")
